6e128c97770cc0484f961556fd74317f80a3f4b5
Drew Keller <dkeller@pillartechnology.com>
2016-09-12 14:45:18 -0400

    Reject IVV as an invalid operand

--- test/invalid-operand-error.c
+++ test/invalid-operand-error.c
@@ -12,10 +12,17 @@ START_TEST(IIV_is_an_invalid_operand) {
   ck_assert_int_eq(expected, actual);
 } END_TEST
 
+START_TEST(IVV_is_an_invalid_operand) {
+  int expected = ROMCALC_INVALID_OPERAND_ERROR;
+  int actual = add(sum, "IVV", "I");
+  ck_assert_int_eq(expected, actual);
+} END_TEST
+
 TCase *invalid_operand_error(void) {
   TCase *invalid_operand_error = tcase_create("Invalid Operand Error");
 
   tcase_add_test(invalid_operand_error, IIV_is_an_invalid_operand);
+  tcase_add_test(invalid_operand_error, IVV_is_an_invalid_operand);
 
   return invalid_operand_error;
 }
