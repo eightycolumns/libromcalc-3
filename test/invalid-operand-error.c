d10e49870974d075e12e6b5a9b9df6f9e9c0eac8
Drew Keller <dkeller@pillartechnology.com>
2016-09-12 14:46:43 -0400

    Reject VX as an invalid operand

--- test/invalid-operand-error.c
+++ test/invalid-operand-error.c
@@ -18,11 +18,18 @@ START_TEST(IVV_is_an_invalid_operand) {
   ck_assert_int_eq(expected, actual);
 } END_TEST
 
+START_TEST(VX_is_an_invalid_operand) {
+  int expected = ROMCALC_INVALID_OPERAND_ERROR;
+  int actual = add(sum, "VX", "I");
+  ck_assert_int_eq(expected, actual);
+} END_TEST
+
 TCase *invalid_operand_error(void) {
   TCase *invalid_operand_error = tcase_create("Invalid Operand Error");
 
   tcase_add_test(invalid_operand_error, IIV_is_an_invalid_operand);
   tcase_add_test(invalid_operand_error, IVV_is_an_invalid_operand);
+  tcase_add_test(invalid_operand_error, VX_is_an_invalid_operand);
 
   return invalid_operand_error;
 }
