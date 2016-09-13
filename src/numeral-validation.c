e2d925f820262414eb37d86ef2df55674048243c
Drew Keller <dkeller@pillartechnology.com>
2016-09-13 16:31:06 -0400

    Extract includes_lowercase()

--- src/numeral-validation.c
+++ src/numeral-validation.c
@@ -10,6 +10,7 @@
 
 static bool is_empty_string(const char *string);
 static bool is_mixed_case(const char *string);
+static bool includes_lowercase(const char *string);
 static bool illegal_characters(const char *substring);
 static bool illegal_order(const char *previous, const char *current);
 static bool illegal_repetition(const char *previous, const char *current);
@@ -75,20 +76,27 @@ static bool is_empty_string(const char *string) {
 static bool is_mixed_case(const char *string) {
   assert(string != NULL);
 
-  bool string_includes_lowercase = false;
   bool string_includes_uppercase = false;
 
   for (size_t i = 0; i < strlen(string); i += 1) {
-    if (islower(string[i])) {
-      string_includes_lowercase = true;
-    }
-
     if (isupper(string[i])) {
       string_includes_uppercase = true;
     }
   }
 
-  return string_includes_lowercase && string_includes_uppercase;
+  return includes_lowercase(string) && string_includes_uppercase;
+}
+
+static bool includes_lowercase(const char *string) {
+  assert(string != NULL);
+
+  for (size_t i = 0; i < strlen(string); i += 1) {
+    if (islower(string[i])) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 static bool illegal_characters(const char *substring) {
